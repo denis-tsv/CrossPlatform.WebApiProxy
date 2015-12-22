ac4134e9bfb20a034efe89da0560dee7e7ea2685
denis-tsv <yadeniskotcvet@gmail.com>
2015-12-22 22:18:51 +0700

    Not commited files

---
+++ Prism.AdventureWorksShopper/AdventureWorks.UILogic.Tests/Mocks/MockFlyoutService.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Prism.StoreApps.Interfaces;
+
+namespace AdventureWorks.UILogic.Tests.Mocks
+{
+    public class MockFlyoutService : IFlyoutService
+    {
+        public Action<string, object, Action> ShowFlyoutDelegate { get; set; }
+
+        public Func<string, FlyoutView> FlyoutResolver { get; set; }
+
+        public void ShowFlyout(string flyoutId)
+        {
+            ShowFlyoutDelegate(flyoutId, null, null);
+        }
+
+        public void ShowFlyout(string flyoutId, object parameter, Action successAction)
+        {
+            ShowFlyoutDelegate(flyoutId, parameter, successAction);
+        }
+    }
+}
